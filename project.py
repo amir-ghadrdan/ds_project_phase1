8699baaa443fb14382e894e52bc363cb15d5b64d
amir_ghadrdan <amirhsynqdrdan81@gmail.com>
2023-12-16 17:00:36 +0330

    2

--- project.py
+++ project.py
@@ -1,5 +1,8 @@
 import re
 import json
+from math import log
+from collections import Counter
+
 
 
 def tokenizer(text):
@@ -7,21 +10,19 @@ def tokenizer(text):
 
 
 def set_list(my_list):
-    my_dict = {}
-    for item in my_list:
-        my_dict[tokenizer(item)] = True
-    unique_list = list(my_dict.keys())
-    return (str(unique_list))
+
+    return dict(Counter(my_list))
 
 
 def open_doc_read(num):
     with open(f'data\\document_{num}.txt', "r", encoding="utf-8") as doc:
-        string = doc.read()
+        string = tokenizer(doc.read())
         patern = "[,. :;\n|]+"
         my_list = re.split(patern, string)
-
-    with open(f'file_data\\file_{num}.txt', 'x', encoding="utf-8") as file:
-        file.write(set_list(my_list))
+    print(set_list(my_list))
+    print(len(set_list(my_list).keys()))
+    # with open(f'file_data\\file_{num}.txt', 'x', encoding="utf-8") as file:
+    #     file.write(set_list(my_list))
 
 
 def open_json():
@@ -33,4 +34,4 @@ def tf_idf():
     pass
 
 
-open_json()
+open_doc_read(0)
