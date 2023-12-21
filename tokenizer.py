fb8dda5a31ac54a44ba835624fa4e1120c63d919
Alisalimi11 <www.asalimi1399@gmail.com>
2023-12-21 14:06:35 +0330

    word changed to word.lower()

--- tokenizer.py
+++ tokenizer.py
@@ -8,5 +8,5 @@ def tokenizer(text):
     text_without_punctuation = text.translate(translation_table)
     stop_words = set(stopwords.words('english'))
     words = word_tokenize(text_without_punctuation)
-    filtered_words = [word for word in words if word.lower() not in stop_words]
+    filtered_words = [word.lower() for word in words if word.lower() not in stop_words]
     return filtered_words
